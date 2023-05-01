905ddfd7dc3b20dabfdaf6ddd67d2560bc0c4981
jongwonpc <c3392302@naver.com>
2023-05-02 07:49:30 +0900

    test: 두 번 게임 했으니 position의 값은 0과 2 사이여야 한다.

--- src/test/java/racingcar/domain/GamingCarsTest.java
+++ src/test/java/racingcar/domain/GamingCarsTest.java
@@ -7,11 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.service.GamingCarService;
 
 class GamingCarsTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("이름 세 개를 작성했으니 gamingCars의 사이즈도 3개여야 한다.")
     void testGamingCars() throws Exception{
         //given
         List<String> cars = Arrays.asList("one","two","three");
@@ -19,6 +20,21 @@ class GamingCarsTest {
         GamingCars gamingCars = new GamingCars(cars);
         //then
         assertEquals(3,gamingCars.getAllCars().size());
+    }
+
+    @Test
+    @DisplayName("두 번 이동했으니 position의 값은 0과2사이여야 한다.")
+    void testGamingCarsMoving() throws Exception{
+        //given
+        List<String> cars = Arrays.asList("one");
+        //when
+        GamingCars gamingCars = new GamingCars(cars);
+        GamingCarService gamingCarService = new GamingCarService(gamingCars);
+        //then
+        gamingCarService.playOneTime();
+        gamingCarService.playOneTime();
+        int position = gamingCars.getAllCars().get(0).getPosition();
+        assertTrue(position >= 0 && position <= 2);
 
     }
 
