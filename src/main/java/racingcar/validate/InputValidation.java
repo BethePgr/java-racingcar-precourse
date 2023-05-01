6f095b30519d388129823714fc8f1f835831c748
SEC <c3392302@naver.com>
2023-05-01 13:46:25 +0900

    fix: 자동차 이름들 유효성 검사 범위 픽스

--- src/main/java/racingcar/validate/InputValidation.java
+++ src/main/java/racingcar/validate/InputValidation.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class InputValidation {
 
     public static void checkInputCarNames(String carNames){
-        if(Arrays.stream(carNames.split(",")).anyMatch(num -> num.length()<=1 || num.length()>=5)){
+        if(Arrays.stream(carNames.split(",")).anyMatch(num -> num.length()<1 || num.length()>5)){
             throw new IllegalArgumentException("[ERROR] 모든 자동차 이름은 1글자와 5글자 사이여야 합니다.");
         }
     }
