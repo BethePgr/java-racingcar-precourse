053e2663d66d57731383435fe954a6a770e95c9c
SEC <c3392302@naver.com>
2023-05-01 13:42:38 +0900

    feat: 자동차 이름들 유효성 검사하기

---
+++ src/main/java/racingcar/validate/InputValidation.java
@@ -0,0 +1,12 @@
+package racingcar.validate;
+
+import java.util.Arrays;
+
+public class InputValidation {
+
+    public static void checkInputCarNames(String carNames){
+        if(Arrays.stream(carNames.split(",")).anyMatch(num -> num.length()<=1 || num.length()>=5)){
+            throw new IllegalArgumentException("[ERROR] 모든 자동차 이름은 1글자와 5글자 사이여야 합니다.");
+        }
+    }
+}
