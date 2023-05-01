29fdc8bc419dd93e1adf3d7ddc9d99f1a4d49ef7
SEC <c3392302@naver.com>
2023-05-01 13:53:27 +0900

    feat: 시도할 횟수 유효성 검사하기

--- src/main/java/racingcar/validate/InputValidation.java
+++ src/main/java/racingcar/validate/InputValidation.java
@@ -9,4 +9,11 @@ public class InputValidation {
             throw new IllegalArgumentException("[ERROR] 모든 자동차 이름은 1글자와 5글자 사이여야 합니다.");
         }
     }
+
+    public static void checkInputGamesTimes(String input){
+        String reg = "^[0-9]+$";
+        if(!input.matches(reg)){
+            throw new IllegalArgumentException("[ERROR] 자동차 게임 시도 횟수는 숫자여야 합니다.");
+        }
+    }
 }
